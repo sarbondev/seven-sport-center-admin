cebe49ccf64903f1c81878895d1853f429054c20
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2025-03-19 11:43:09 +0500

    render link

--- src/middlewares/Axios.tsx
+++ src/middlewares/Axios.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 const token = localStorage.getItem("ssctoken") || "";
 
 export const Axios = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  // baseURL: "http://localhost:3001/api/",
+  baseURL: "https://seven-sport-center-server.onrender.com/api/",
   headers: {
     Authorization: token,
   },
--- src/middlewares/Fetcher.tsx
+++ src/middlewares/Fetcher.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:3001/api";
+// export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "https://seven-sport-center-server.onrender.com/api";
 const token = localStorage.getItem("ssctoken") || "";
 
 export const fetcher = (url: string) =>
