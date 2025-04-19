36f2ad3c273f418afd8393c809585ac231c47d5c
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2025-04-19 12:56:29 +0500

    Prepared to deploy on vercel

--- src/middlewares/Fetcher.tsx
+++ src/middlewares/Fetcher.tsx
@@ -1,5 +1,6 @@
-export const BASE_URL = "http://localhost:3001/api";
+// export const BASE_URL = "http://localhost:3001/api";
 // export const BASE_URL = "https://seven-sport-center-server.onrender.com/api";
+export const BASE_URL = "https://server.7sportcenter.uz/api";
 const token = localStorage.getItem("ssctoken") || "";
 
 export const fetcher = (url: string) =>
